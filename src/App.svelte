f88ace90c77820b4ab45328ebb1c3e03bb4ac90f
ItsKruistz <lekruistz@gmail.com>
2024-07-28 20:49:00 +0700

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,13 @@
 <main>
 
 <nav class="flex items-center justify-between flex-wrap bg-white shadow sticky z-10 top-0 p-2">
-  <div class="flex items-center flex-shrink-0 text-black mr-6">
+  <div class="flex items-center flex-shrink-0 text-black">
     <button class="flex items-center justify-center h-11 w-11 rounded-full hover:bg-neutral-100">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current" viewBox="0 -960 960 960">
         <path d="M160-240q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Zm0-200q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Zm0-200q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Z"/>
       </svg>
     </button>
     <span class="font-medium text-xl ml-2.5 block">Title</span>
-  </div>
-  <div>
-    <button class="flex items-center justify-center h-11 w-11 rounded-full hover:bg-neutral-100">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current" viewBox="0 -960 960 960">
-        <path d="M380-320q-109 0-184-75t-76-185q0-109 76-184t184-76q109 0 185 76t75 184q0 44-14 83t-38 69l224 224q11 11 11 28t-11 28q-11 11-28 11t-28-11L532-372q-30 24-69 38t-83 14Zm0-80q75 0 128-52t52-128q0-75-52-127t-128-53q-75 0-127 53t-53 127q0 75 53 128t127 52Z"/>
-      </svg>
-    </button>
-  </div>
-    <button class="flex items-center justify-center h-11 w-11 rounded-full hover:bg-neutral-100">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current" viewBox="0 -960 960 960">
-        <path d="M380-320q-109 0-184-75t-76-185q0-109 76-184t184-76q109 0 185 76t75 184q0 44-14 83t-38 69l224 224q11 11 11 28t-11 28q-11 11-28 11t-28-11L532-372q-30 24-69 38t-83 14Zm0-80q75 0 128-52t52-128q0-75-52-127t-128-53q-75 0-127 53t-53 127q0 75 53 128t127 52Z"/>
-      </svg>
-    </button>
-  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
   </div>
 </nav>
 
