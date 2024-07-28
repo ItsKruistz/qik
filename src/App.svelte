c6608bfae4e556720bf3ebd82b7ab86eb46fc109
ItsKruistz <lekruistz@gmail.com>
2024-07-28 08:49:54 +0700

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <main>
 
-<nav class="flex items-center justify-between flex-wrap bg-white sticky z-10 top-0 p-2">
-  <div class="flex items-center flex-shrink-0 text-black mr-6">
+<nav class="flex items-center justify-between flex-wrap bg-teal-500 sticky z-10 top-0 p-2">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <button class="inline-flex items-center justify-center whitespace-nowrap rounded-lg text-[15px] font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-11 w-11">OMG</button>
     <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
     <span class="font-medium text-xl">Kruistz's Page</span>
   </div>
