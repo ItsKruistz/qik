54c179ddee3ddb71e79a2f279b43b4ee6471a803
ItsKruistz <lekruistz@gmail.com>
2024-07-28 10:34:37 +0700

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <main>
 
-<nav class="flex items-center justify-between flex-wrap bg-teal-500 sticky z-10 top-0 p-2">
-  <div class="flex items-center flex-shrink-0 text-white mr-6">
-    <button class="flex items-center justify-center h-11 w-11 rounded-full text-white hover:bg-teal-400">
+<nav class="flex items-center justify-between flex-wrap bg-white sticky z-10 top-0 p-2">
+  <div class="flex items-center flex-shrink-0 text-black mr-6">
+    <button class="flex items-center justify-center h-11 w-11 rounded-full hover:bg-teal-400">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current" viewBox="0 -960 960 960">
         <path d="M160-240q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Zm0-200q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Zm0-200q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Z"/>
       </svg>
@@ -10,7 +10,7 @@
     <span class="font-medium text-xl ml-2.5">I need money</span>
   </div>
   <div class="block lg:hidden">
-    <button class="flex items-center justify-center h-11 w-11 rounded-full text-white hover:bg-teal-400">
+    <button class="flex items-center justify-center h-11 w-11 rounded-full hover:bg-teal-400">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current" viewBox="0 -960 960 960">
         <path d="M160-240q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Zm0-200q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Zm0-200q-17 0-28-11t-12-29q0-17 12-28t28-12h640q17 0 29 12t11 28q0 17-11 29t-29 11H160Z"/>
       </svg>
